92f2131ce5289e079f40a1bdbf42a76d581af63b
github username <github email address>
2017-12-12 17:17:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// function BoardMember(name, homeState, training){
-//   this.name = name
-//   this.homeState = homeState
-//   this.training = training
-//
-//   this.veto = function(){
-//     return 'No, I must disagree'
-//   }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
+
+  this.veto => {
+    return 'No, I must disagree'
+  }
+  this.approve => {
+    return 'You can do that!'
+  }
+  this.doCharity => {
+    return "I like to help people."
+  }
+
+  this.releasePressStatement => {
+    return "You will see great things from Scuber."
+  }
+
+  this.sayHi => {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+}
